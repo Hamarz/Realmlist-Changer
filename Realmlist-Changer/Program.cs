27412c7fe5de10005d3112b630090afef604cbc2
Discover- <amort11@hotmail.com>
2014-01-13 12:22:36 +0100

    When the user starts the Realmlist-Changer while there is already one running, it now focuses the open one instead of opening a new one

--- Realmlist-Changer/Program.cs
+++ Realmlist-Changer/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +11,41 @@ namespace Realmlist_Changer
 {
     static class Program
     {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormRealmlistChanger());
+            bool createdNew = true;
+
+            using (Mutex mutex = new Mutex(true, "Realmlist-Changer", out createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new FormRealmlistChanger());
+                }
+                else
+                {
+                    Process current = Process.GetCurrentProcess();
+
+                    foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+                    {
+                        if (process.Id != current.Id)
+                        {
+                            MessageBox.Show("Setting foreground!");
+                            SetForegroundWindow(process.MainWindowHandle);
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
