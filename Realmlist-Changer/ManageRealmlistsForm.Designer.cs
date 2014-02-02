d496a45b839ffbceba84e2e0ec7804b0c8d9b7fb
Discover- <amort11@hotmail.com>
2014-02-02 15:56:59 +0100

    Get rid of the maximize box in the ManageRealmlistsForm

--- Realmlist-Changer/ManageRealmlistsForm.Designer.cs
+++ Realmlist-Changer/ManageRealmlistsForm.Designer.cs
@@ -117,6 +117,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
+            this.MaximizeBox = false;
             this.Name = "ManageRealmlistsForm";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
